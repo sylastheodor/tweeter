4b13ed52f6409de63a66f21ffa49d6a14b457266
Sylas Serné <its.sylas@gmail.com>
2021-07-14 23:34:22 +0000

    Okay so all that stuff I did in jquery I could actually just do in CSS with the :hover pseudo-class lol.  tweetBehaviour.js removed.

--- public/scripts/tweetBehaviour.js
+++ public/scripts/tweetBehaviour.js
@@ -3,35 +3,5 @@ $(document).ready(function() {
   const retweet = document.querySelector('i.fa-retweet');
   const heart = document.querySelector('i.fa-heart');
   const tweet = document.querySelector("article[name='tweet']");
-  $(tweet).on('mouseover', function() {
-    $(tweet).css("box-shadow", "5px 4px #58554c42")
-  })
-  $(tweet).on('mouseout', function() {
-    $(tweet).css("box-shadow", "0px 0px")
-  })
-  $(flag).on('mouseover', function() {
-    $(flag).css("color", "#008DFF")
-    $(flag).css("transform", "scale(1.2)")
-  })
-  $(flag).on('mouseout', function() {
-    $(flag).css("color", "")
-    $(flag).css("transform", "scale(1)")
-  })
-  $(retweet).on('mouseover', function() {
-    $(retweet).css("color", "#008DFF")
-    $(retweet).css("transform", "scale(1.2)")
-  })
-  $(retweet).on('mouseout', function() {
-    $(retweet).css("color", "")
-    $(retweet).css("transform", "scale(1)")
-  })
-  $(heart).on('mouseover', function() {
-    $(heart).css("color", "#008DFF")
-    $(heart).css("transform", "scale(1.2)")
-  })
-  $(heart).on('mouseout', function() {
-    $(heart).css("color", "")
-    $(heart).css("transform", "scale(1)")
-  })
-
+  
 });

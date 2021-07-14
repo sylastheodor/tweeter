a82488bc3e5c4faf30eb66d0a0a3bcfbf42746c0
Sylas Serné <its.sylas@gmail.com>
2021-07-14 23:08:22 +0000

    initial styling for the way the tweet interacts completed.  Ideally I would like to make the buttons do cuter stuff when moused over like a cute little wiggle

--- public/scripts/tweetBehaviour.js
+++ public/scripts/tweetBehaviour.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
-  const icons = document.querySelector('div.icons');
-  const tweet = document.querySelector("article[name='tweet']")
+  const flag = document.querySelector('i.fa-flag');
+  const retweet = document.querySelector('i.fa-retweet');
+  const heart = document.querySelector('i.fa-heart');
+  const tweet = document.querySelector("article[name='tweet']");
   $(tweet).on('mouseover', function() {
     $(tweet).css("box-shadow", "5px 4px #58554c42")
   })
   $(tweet).on('mouseout', function() {
     $(tweet).css("box-shadow", "0px 0px")
   })
-  $(icons)
+  $(flag).on('mouseover', function() {
+    $(flag).css("color", "#008DFF")
+    $(flag).css("transform", "scale(1.2)")
+  })
+  $(flag).on('mouseout', function() {
+    $(flag).css("color", "")
+    $(flag).css("transform", "scale(1)")
+  })
+  $(retweet).on('mouseover', function() {
+    $(retweet).css("color", "#008DFF")
+    $(retweet).css("transform", "scale(1.2)")
+  })
+  $(retweet).on('mouseout', function() {
+    $(retweet).css("color", "")
+    $(retweet).css("transform", "scale(1)")
+  })
+  $(heart).on('mouseover', function() {
+    $(heart).css("color", "#008DFF")
+    $(heart).css("transform", "scale(1.2)")
+  })
+  $(heart).on('mouseout', function() {
+    $(heart).css("color", "")
+    $(heart).css("transform", "scale(1)")
+  })
 
 });

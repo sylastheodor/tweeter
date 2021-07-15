5a8b92581edc42c8f2fd137b074ff5531b2e15c7
Sylas Serné <its.sylas@gmail.com>
2021-07-15 02:07:02 +0000

    JS in browser branch looks good to me

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,21 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const $tweet=$(`<article class="tweet">stan loona</article>`);
 
+const createTweetElement = function(tweetData) {
+  const tweetStructure = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227}
+}
+
+$(document).ready(function() {
+
+})
